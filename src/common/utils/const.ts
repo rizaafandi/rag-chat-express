aed5bb1534b7001eb775d45090b7c2f3c4a394cf
rafandi <rizaaffandi45@gmail.com>
2025-06-16 23:00:57 +0700

    Add website into source

--- src/common/utils/const.ts
+++ src/common/utils/const.ts
@@ -1,4 +1,3 @@
-
 import { env } from "@/common/utils/envConfig";
 
 export const HF_TOKEN = env.HUGGINGFACE_API_KEY;
@@ -9,19 +8,24 @@ export const QDRANT_COLLECTION = env.QDRANT_COLLECTION;
 export const QDRANT_VECTORE_SIZE = 768; // for all-MiniLM-L6-v2
 export const GEMINI_API_KEY = env.GEMINI_API_KEY;
 
-
-export const HF_EMBEDDING_MODEL = 'sentence-transformers/all-MiniLM-L6-v2';
-export const LOCAL_EMBEDDING_MODEL = 'Xenova/all-MiniLM-L6-v2';
-export const LOCAL_PIPELINE = 'feature-extraction';
-export const LLM_MODEL = 'mistralai/Mistral-Small-3.1-24B-Instruct-2503';
+export const HF_EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2";
+export const LOCAL_EMBEDDING_MODEL = "Xenova/all-MiniLM-L6-v2";
+export const LOCAL_PIPELINE = "feature-extraction";
+export const LLM_MODEL = "mistralai/Mistral-Small-3.1-24B-Instruct-2503";
 export const TOP_K = 3;
 
 export interface Document {
-  id: string;
-  text: string;
-  metadata: {
-    source: string;
-    page?: number;
-    [key: string]: any;
-  };
+	id: string;
+	text: string;
+	metadata: {
+		source: string;
+		page?: number;
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		[key: string]: any;
+	};
 }
+
+export const whitelistWebsite = [
+	"https://www.ncbi.nlm.nih.gov/books/NBK554776/",
+	"https://opencovidjournal.com/VOLUME/4/ELOCATOR/e26669587296962/FULLTEXT/",
+];
