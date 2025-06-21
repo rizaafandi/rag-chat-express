516238c76aaaeb2e40dd7aacf43144f1eee7adf2
rafandi <rizaaffandi45@gmail.com>
2025-06-21 22:43:05 +0700

    Remove Limiter

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-app.use(rateLimiter);
+// app.use(rateLimiter);
 
 // Request logging
 app.use(requestLogger);
